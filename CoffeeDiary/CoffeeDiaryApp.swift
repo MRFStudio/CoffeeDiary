49bac85a449d503562b62328bb3a96ade64fe1f8
MJ Michand <01-loners.strait@icloud.com>
2024-12-05 08:22:39 -0500

    Initial Commit

---
+++ CoffeeDiary/CoffeeDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDiaryApp.swift
+//  CoffeeDiary
+//
+//  Created by MJ Michand on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeDiary
+//
+//  Created by MJ Michand on 12/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
